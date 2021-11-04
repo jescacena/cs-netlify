34f482d91929d4a1158001bb5734f22335aac64a
Javier Escacena Sánchez <fjescacena@paradigmadigital.com>
2021-11-04 14:39:44 +0100

    content update

---
+++ _posts/2021-11-04-react-native.markdown
@@ -0,0 +1,10 @@
+---
+layout: category-post
+title:  React Native
+date:   2020-06-13T11:50:54.194Z
+permalink: /react-native/
+categories: [catpost]
+icon: https://codersnack.com/assets/images/react-native-icon.png
+exclude: true
+---
+ * [What is React Native?](https://codersnack.com/react-native-what-is) 
--- strapi-snacks-to-category-posts.js
+++ strapi-snacks-to-category-posts.js
@@ -177,7 +177,7 @@ const writeCategoryPost = data => {
 
 Promise.all(
     categoryUrlMap.map(item =>
-        fetch(baseCMSUrl + item.urlPath + '?_limit=10000')
+        fetch(baseCMSUrl + item.urlPath + '&_limit=10000')
             .then(response => response.json())
             .then(response => {
                 return {
